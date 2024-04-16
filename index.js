6bfef4595797f1312856dc31b887abcd3f932fbb
qminhminh <hqminh050503@gmail.com>
2024-04-16 18:52:42 +0700

    phan 9: backend catrgory and controller

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
@@ -10,5 +10,5 @@ mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("Foodly Database Connected"))
 .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`));
