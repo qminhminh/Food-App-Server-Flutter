9f9c55e54ead6d36a62fb91f84618cc9aa759934
qminhminh <hqminh050503@gmail.com>
2024-04-16 22:50:22 +0700

    phan 14: user schema and auth token and otp

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const CategoryRoute = require('./routes/category');
 const RestaurantRoute = require("./routes/restaurant");
 const FoodRoute = require("./routes/food");
 const RatingRoute = require("./routes/rating");
+const AuthRoute = require("./routes/auth");
+const UserRoute = require("./routes/user");
+
+
 dotenv.config();
 
 
@@ -15,11 +19,16 @@ mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("Foodly Database Connected"))
 .catch((err) => console.log(err));
 
+
 // 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
+// auth routes
+app.use("/", AuthRoute);
+//user routes
+app.use("/api/users", UserRoute);
 // Category routes
 app.use("/api/category", CategoryRoute);
 // Restaurant routes
