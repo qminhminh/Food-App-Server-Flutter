a3a462c78f94f86e45378d69157ac0d37ae79c1d
qminhminh <hqminh050503@gmail.com>
2024-04-16 19:58:47 +0700

    phan 11: restaurant schema and controller, route

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
-
+const morgan = require('morgan');
+const CategoryRoute = require('./routes/category');
+const RestaurantRoute = require("./routes/restaurant")
 dotenv.config();
 
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("Foodly Database Connected"))
 .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`));
+// 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+// Category routes
+app.use("/api/category", CategoryRoute);
+// Restaurant routes
+app.use("/api/restaurant", RestaurantRoute);
+
+
+// Listen to port
+app.listen(process.env.PORT || 3000, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`));
