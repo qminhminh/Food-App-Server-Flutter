fc83a7c414dbec6a372f4abf8885347af51d5b6d
qminhminh <hqminh050503@gmail.com>
2024-04-19 10:37:21 +0700

    add fcm in uẻ model

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     otp: {type: String, required: false, default: "none"},
+    fcm: {type: String, required: false, default:"none"},
     password: {type: String, required: true},
     verification: {type: Boolean, default: false},
     phone: {type: String, default: "0123456789"},
