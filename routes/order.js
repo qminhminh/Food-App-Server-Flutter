c5d8c9f05d99ca5641632e79c289a0c7b36231a3
qminhminh <hqminh050503@gmail.com>
2024-05-19 10:19:36 +0700

    add oder

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,12 @@ const {verifyTokenAndAuthorization} = require('../middleware/verifyToken');
 
 router.post("/",verifyTokenAndAuthorization, orderController.placeOrder);
 
+router.get("/:id",verifyTokenAndAuthorization, orderController.getOrderDetails);
+
 router.get("/",verifyTokenAndAuthorization, orderController.getUserOrders);
+
+router.get("/rest-orders/:id/:status",verifyTokenAndAuthorization, orderController.getRestaurantOrder);
+
+router.put("/update/:id",verifyTokenAndAuthorization, orderController.updateOrderStatus);
 
 module.exports = router;
